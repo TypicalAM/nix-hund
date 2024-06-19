defd59c71250012fec53f897a2517af489273797
TypicalAM <tpam_piaseczny@protonmail.com>
2024-06-19 19:35:40 +0200

    build: better flake and Dockerfile

--- backend/flake.nix
+++ backend/flake.nix
@@ -1,18 +1,45 @@
 {
   description = "Locate nix development files easily";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go
-            gopls
-          ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem  (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        # Development shell
+        devShell = mkShell {
+          name = "nix-hund";
+          nativeBuildInputs = [ go gopls ];
         };
-      };
-    };
+
+        # Runtime package
+        packages.nix-hund = pkgs.buildGoModule rec {
+          pname = "nix-hund";
+          version = "0.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "TypicalAM";
+            repo = "nix-hund";
+            rev = "v${version}";
+            hash = "sha256-vXyrZWsF6/6EGJ+cT1L0Q7h6FcP0/HFaRLjNFrzPUmU=";
+          } + "/backend";
+
+          vendorHash = "sha256-v4Y6CUxTz59GX7GI8zfI7RsC24/aS0aHW6s6nQzRBkA=";
+
+          meta = {
+            description = "Locate nix development files easily";
+            homepage = "https://github.com/TypicalAM/nix-hund";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ TypicalAM ];
+          };
+        };
+
+        # Default package
+        defaultPackage = packages.nix-hund;
+      }
+    );
 }
